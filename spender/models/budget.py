83046ad834e2d7b80e0e530ef80d341be7e8a933
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2018-05-16 18:15:33 +0200

    small changes

--- spender/models/budget.py
+++ spender/models/budget.py
@@ -12,17 +12,12 @@ class Budget(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     # the data type of the budget should match the data type of the price
     budget = Column(Numeric(15, 2))
-    category_id = Column(Integer, ForeignKey('categories.id'))
     budget_userid = Column(Integer, ForeignKey('users.id'))
-    budget_start_date = Column(DateTime)
-    budget_end_date = Column(DateTime)
-
     user = relationship("User", backref=backref('budget'))
-
-    category = relationship("Category", backref=backref('budget'))
 
     def __repr__(self):
         """ Provide useful info """
 
-        return "<Budget id=%s budget=%s budget_userid=%s category=%s budget_start_date=%s budget_end_date=%s>" % (
-            self.id, self.budget, self.budget_userid, self.category, self.budget_start_date, self.budget_end_date)
+        return "<Budget id=%s budget=%s budget_userid=%s\
+                category=%s budget_start_date=%s budget_end_date=%s>" % (
+                self.id, self.budget, self.budget_userid)
